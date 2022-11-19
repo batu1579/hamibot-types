cae77b64ed09f3741b3412e0fd6c095765a9b5df
BATU1579 <batu1234567890@outlook.com>
2022-11-19 15:31:06 +0800

    ✨ feat: 添加更新脚本判断版本的功能
    现在可以更新脚本可以自己判断是否需要下载新的版本了

--- scripts/updateTypes.ps1
+++ scripts/updateTypes.ps1
@@ -1,17 +1,45 @@
 $tempFile = "types.tar.gz"
-
 $typeFolder = "hamibot-types"
 
-$releasesInfo = Invoke-WebRequest -Uri "https://api.github.com/repos/batu1579/hamibot-types/releases/latest"
-
-$releasesInfo.content -match '"tarball_url":"(?<tarUrl>.*?)"'
-
-curl -L --ssl-no-revoke -o $tempFile $Matches.tarUrl
-
 if (-not (Test-Path -Path $typeFolder)) {
-    mkdir $typeFolder
+    $null = New-Item -path $typeFolder -type directory
 }
 
-tar -zxvf $tempFile -C $typeFolder --strip-components 1
+echo "Checking update for types..."
+echo ""
+
+# 获取最新标签名
+$response = Invoke-WebRequest -Uri "https://api.github.com/repos/batu1579/hamibot-types/releases/latest"
+$null = $response.content -match '"tag_name":"(?<tag_name>.*?)"'
+$latestVersion = $Matches.tag_name
+
+$versionFile = "${typeFolder}/version.txt"
+
+if (Test-Path -Path $versionFile) {
+    $currentVersion = $(Get-Content $versionFile)
+    echo "current version: ${currentVersion}"
+    echo "latest version: ${latestVersion}"
+    echo ""
+    if ($latestVersion -eq $currentVersion) {
+        echo "The latest version is being used"
+        exit
+    }
+} else {
+    echo "latest version: ${latestVersion}"
+    echo ""
+    $null = New-Item $versionFile
+}
+
+echo "Donwloading update..."
+echo ""
+
+curl -Ls --ssl-no-revoke -o  $tempFile "https://github.com/batu1579/hamibot-types/releases/download/${latestVersion}/hamibot-types.tar.gz"
+
+tar -zxf $tempFile -C $typeFolder
 
 Remove-Item $tempFile
+
+echo $latestVersion > $versionFile
+echo "Done !"
+
+# tar -zxvf $tempFile -C $typeFolder --strip-components 1
